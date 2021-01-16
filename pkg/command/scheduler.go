6eb5fd2ac918b82542c1fd4ae3a31c8ffd8dab56
Naoki Kanazawa <n-kanazawa@sakura.ad.jp>
2021-01-16 19:00:04 +0900

    schedulerにconfigを読み込ませるベースを作成

--- pkg/command/scheduler.go
+++ pkg/command/scheduler.go
@@ -1,6 +1,12 @@
 package command
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
 
 func NewSchedulerCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -14,15 +20,44 @@ func NewSchedulerCommand() *cobra.Command {
 	return cmd
 }
 
+type schedulerConfig struct {
+}
+
 func NewSchedulerStartCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "start",
 		RunE: schedulerStartCommandFunc,
 	}
 
+	flags := cmd.Flags()
+	flags.StringP("config", "c", "", "config file path")
+
+	cmd.MarkFlagRequired("config")
+
 	return cmd
 }
 
 func schedulerStartCommandFunc(cmd *cobra.Command, args []string) error {
+	flags := cmd.Flags()
+	configPath, err := flags.GetString("config")
+	if err != nil {
+		return err
+	}
+
+	// read mapping file
+	bytes, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return err
+	}
+
+	cfg := []schedulerConfig{}
+	if err := yaml.Unmarshal(bytes, &cfg); err != nil {
+		return err
+	}
+
+	fmt.Printf("[INFO] config: %#v\n", cfg)
+
+	// schedulerの起動
+
 	return nil
 }
