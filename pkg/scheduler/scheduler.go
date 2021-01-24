245e031201a8da962c30f0055a49791ce10b9a2e
yatuhashi <yatuhashi@alice-tech.net>
2021-01-24 19:13:50 +0900

    fix not exist code name 2

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -70,7 +70,7 @@ func AggregateInstance(pis map[string]*types.ProblemInstance, zps []types.ZonePr
 	for _, p := range *pes {
 		min := strings.Split(*p.MachineImageName, "-")
 		pn := min[0]
-		if _, ok := pis[pn]; ok {
+		if _, ok := pis[pn]; !ok {
 			lg.Error("This problem name not exists. The value is " + pn)
 			continue
 		}
